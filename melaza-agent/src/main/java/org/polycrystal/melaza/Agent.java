3a5bdfdb389c11c8f2cab43234cb28331c5e3a60
Patrick Mahoney <pat@polycrystal.org>
2013-01-01 17:35:41 -0600

    Remove unused (old) properties file load.

--- melaza-agent/src/main/java/org/polycrystal/melaza/Agent.java
+++ melaza-agent/src/main/java/org/polycrystal/melaza/Agent.java
@@ -37,29 +37,6 @@ public class Agent {
     public static void premain(String args, Instrumentation inst) {
         final Config config = new Config(args);
         configureLogger(config);
-
-        Properties props = null;
-        
-        if (args == null || args.length() == 0 || args.equals("null")) {
-            props = new Properties();
-        } else {
-            try {
-                final BufferedInputStream input =
-                        new BufferedInputStream(new FileInputStream(args));
-                try {
-                    props = new Properties();
-                    props.load(input);
-                } catch (IOException e) {
-                    System.err.println("warning: error loading " + args);
-                } finally {
-                    try { input.close(); } catch (IOException e) {}
-                }
-            } catch (FileNotFoundException e1) {
-                System.err.println("warning: no such file " + args);
-                props = new Properties();
-            }
-        }
-        
         inst.addTransformer(new Transformer());
     }
     
